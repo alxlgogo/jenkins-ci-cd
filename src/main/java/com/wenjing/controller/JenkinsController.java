3e5a7c9a334e4cd6f0e5f7b89ae4dfc3098682f6
alxlgogo <alxlgogo@gmail.com>
2022-07-02 18:35:55 +0100

    Finished practies.

--- src/main/java/com/wenjing/controller/JenkinsController.java
+++ src/main/java/com/wenjing/controller/JenkinsController.java
@@ -12,6 +12,11 @@ public class JenkinsController {
 
     @RequestMapping("trigger1")
     public String trigger1(){
-        return "Jenkins is triggered be github!22";
+        return "Jenkins is triggered be github!";
+    }
+
+    @RequestMapping("trigger2")
+    public String trigger2(){
+        return "All done!";
     }
 }
